fbc986f4470167b49e3429e38bbd224c42e67cab
Byron <byron2217@gmail.com>
2018-04-20 12:05:20 +0000

    added passport setup to app.js

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -10,9 +10,21 @@ mongoose.connect("mongodb://localhost/autho_demo");
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(require("express-session")({
+    secret: "Olive is the best and cutest dog ever",
+    resave: false,
+    saveUninitialized: false
+}));
+
 //Setting passport up to work in the app. 2 lines below are required when using passport
 app.use(passport.initialize());
 app.use(passport.session());
+
+//2 lines below responsible for reading the session and taking the data from the session and decoding, then recoding it back into
+//the session. by adding "UserSchema.plugin(passportLocalMongoose);" in user.js this is already setup and ready to use
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", function(req, res){
     res.render("home");
